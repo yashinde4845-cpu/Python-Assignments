3c04b8ff5abce443488b23c4fb353be019931d6e
yashinde4845-cpu <yashinde4845@gmail.com>
2026-05-03 17:03:55 +0530

    Add files via upload

---
+++ Unit - 3/shapes.py
@@ -0,0 +1,11 @@
+# List of Shapes
+import math
+
+def circle_area (radius):
+  return math.pi * radius * radius
+
+def rectangle_area(length ,width):
+  return length * width
+
+def triangle_area(base , height):
+  return 0.5 * base * height
